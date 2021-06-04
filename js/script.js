0ec3abc4ee82d86cbc883f04672bf8b44bbabb69
attila <attilavigh.contact@gmail.com>
2021-06-04 15:55:49 +0100

    Netflix submission.

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const settings = [
+    {
+        col_1: {
+            section: "membership & billing",
+            button : "cacel memebership"
+        }, 
+        col_2: {
+            loginDetails: {
+                username: "student@strive.school",
+                password: "********",
+                phone   : "321 545 6543",
+            },
+            payDetails: {
+                paymentInfo    : "PayPall",
+                billingDetails : "admin@strive.school",
+            }
+        },
+        col_4: {
+            loginDetails: {
+                change_email    : "Chage account email",
+                change_password : "Chage password",
+                change_phone    : "Chage phone number",
+            },
+            payDetails: {
+                paymentInfo   : "Update payment info",
+                billingDetails: "PayPall",
+            }
+        }
+
+]
